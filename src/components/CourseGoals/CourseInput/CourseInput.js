7074da251ada2b37d2fe41ab78b8bc6a09f9dc52
venkatesh <venkatesh9ui@gmail.com>
2021-11-22 00:00:06 -0500

    Styled Components & Dynamic Props - 2

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -11,12 +11,14 @@ const FormControl = styled.div`
   font-weight: bold;
   display: block;
   margin-bottom: 0.5rem;
+  color: ${props => (props.invalid ? 'red' : 'black')};
 }
 
 & input {
   display: block;
   width: 100%;
-  border: 1px solid #ccc;
+  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+  background-color: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
   font: inherit;
   line-height: 1.5rem;
   padding: 0 0.25rem;
@@ -28,14 +30,14 @@ const FormControl = styled.div`
   border-color: #8b005d;
 }
 
-&.invalid input{
-  background-color: #ffd7d7;
-  border-color: red;
-}
+// &.invalid input{
+//   background-color: #ffd7d7;
+//   border-color: red;
+// }
 
-&.invalid label{
-  color: red;
-}
+// &.invalid label{
+//   color: red;
+// }
 `
 
 const CourseInput = props => {
@@ -62,7 +64,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className= {!isValid ? 'invalid' : ''}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
       </FormControl>
